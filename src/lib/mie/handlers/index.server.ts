96ee9a13936454a30bf4056f26597561339c28c5
Bismarckgo <bismarckgarciaolivo@gmail.com>
2026-08-13 15:41:44 -0400

    chore(mie): handlers index to register server handlers on import

---
+++ src/lib/mie/handlers/index.server.ts
@@ -0,0 +1,4 @@
+// Barrel file to ensure server handlers are imported and register themselves
+import "./validation-handler.server";
+import "./inference-handler.server";
+import "./notification-handler.server";
